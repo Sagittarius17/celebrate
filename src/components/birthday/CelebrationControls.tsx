906b05511673babbd51cfe20811e98fbb094c556
Shuvendu Singha <shuvendusingha17@gmail.com>
2026-03-09 16:06:33 +0000

    play button svg size

--- src/components/birthday/CelebrationControls.tsx
+++ src/components/birthday/CelebrationControls.tsx
@@ -255,7 +255,7 @@ export const CelebrationControls: React.FC<CelebrationControlsProps> = ({
                   <div className="w-1.5 h-5 sm:w-2.5 sm:h-8 bg-black rounded-full" />
                 </div>
               ) : (
-                <Play className="fill-black ml-0.5 sm:ml-1 w-7 h-7 sm:w-12 sm:h-12" />
+                <Play className="fill-black ml-0.5 sm:ml-1" style={{ width: '40px', height: '40px' }} />
               )}
             </Button>
           </div>
